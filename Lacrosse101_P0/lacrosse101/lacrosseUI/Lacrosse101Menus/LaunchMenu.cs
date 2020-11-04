19a21b94e25b2e33d79f60606ef929a3271537a3
Rachel Hufnagel <rachelhufnagel_2020@yahoo.com>
2020-11-04 07:33:03 -0600

    das

--- Lacrosse101_P0/lacrosse101/lacrosseUI/Lacrosse101Menus/LaunchMenu.cs
+++ Lacrosse101_P0/lacrosse101/lacrosseUI/Lacrosse101Menus/LaunchMenu.cs
@@ -96,7 +96,6 @@ namespace lacrosseUI.Lacrosse101Menus
                 {
                     customer = cust;
                     Log.Information($"{cust.email} has signed in");
-                    custMenu = new CustMenu(customer, context);
                     try
                     {
                         cartServices.DeleteCart(cartServices.GetCartByCustId(customer.Id));
@@ -107,6 +106,7 @@ namespace lacrosseUI.Lacrosse101Menus
                         Cart newCart = new Cart();
                         newCart.custId = customer.Id;
                         cartServices.AddCart(newCart);
+                        custMenu = new CustMenu(customer, context);
                         custMenu.Start();
                     }
                 }
